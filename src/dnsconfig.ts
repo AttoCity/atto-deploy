51bba8d5ce28d240791f29ec427e324a0d67f39a
oott123 <git@public.oott123.com>
2022-06-25 18:32:08 +0800

    feat: use dns config

---
+++ src/dnsconfig.ts
@@ -0,0 +1,29 @@
+import { resolveTxt } from 'std/node/dns.ts'
+
+export async function getWorkerUrl(domain: string): Promise<string | null> {
+  const baseDomain = '.dcnf.atto.town'
+  const queryDomain = `${domain}${baseDomain}`
+  const result = await new Promise<string[][]>((resolve, reject) => {
+    resolveTxt(queryDomain, (err, records) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(records)
+      }
+    })
+  })
+  if (result.length < 1) {
+    return null
+  }
+  return result.sort().map(normalizeQueryResult).join('')
+}
+
+function normalizeQueryResult(texts: string[]): string {
+  const text = texts.join('')
+  const matches = text.match(/\d+\s+(.*)$/)
+  if (matches) {
+    return matches[1]
+  } else {
+    return text
+  }
+}
--- src/mod.ts
+++ src/mod.ts
@@ -1,20 +1,55 @@
 import { serve, ConnInfo } from 'http/server.ts'
 import { postRequestToWorker } from './bridge/postRequestToWorker.ts'
+import { getWorkerUrl } from './dnsconfig.ts'
+import LRU from 'lru_cache'
+
+const workers = new LRU<string, Worker>({
+  max: 20,
+  dispose(_, worker) {
+    if (worker) {
+      worker.terminate()
+    }
+  },
+  updateAgeOnGet: true,
+  maxAge: 5 * 60 * 1000,
+})
+
+function getWorker(url: string): Worker {
+  const cachedWorker = workers.get(url)
+  if (cachedWorker) {
+    return cachedWorker
+  }
+
+  const worker = new Worker(url, {
+    type: 'module',
+  })
+  workers.set(url, worker)
+
+  worker.onerror = (err) => {
+    console.error(err)
+    workers.del(url)
+  }
+
+  return worker
+}
 
 async function handler(req: Request, connInfo: ConnInfo): Promise<Response> {
-  const workerUrl = req.headers.get('atto-worker')
+  const reqUrl = new URL(req.url)
+  const workerUrl = await getWorkerUrl(reqUrl.hostname)
   if (!workerUrl) {
     return new Response('no worker url given', { status: 400 })
   }
 
-  console.log(workerUrl)
-  const worker = new Worker(workerUrl, {
-    type: 'module',
-    name: 'handler-worker',
-  })
+  if (req.method === 'PURGE' && reqUrl.pathname === '/atto-cgi/purge-worker') {
+    workers.del(workerUrl)
+    return new Response(null, { status: 204 })
+  }
 
+  const worker = getWorker(workerUrl)
   const { responseDefer } = postRequestToWorker(worker, req, connInfo)
-  return await responseDefer.promise
+  const response = await responseDefer.promise
+
+  return response
 }
 
 serve(handler)
